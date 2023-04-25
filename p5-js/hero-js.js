7651f08b3b245eaacafb2d4fec778762a6343925
TheKooperDon <valodinejr@gmail.com>
2023-04-25 09:01:00 -0500

    Adding p5.js folders.
    Still does not work.

--- p5-js/hero-js.js
+++
@@ -1,9 +0,0 @@
-function setup() {
-    var canvas = createCanvas(400, 400);
-    canvas.parent('hero-js');
-  }
-  function draw() {
-    
-    ellipse(mouseX, mouseY, 50, 50);
-    fill(random(255),random(255),random(255))
-  }
