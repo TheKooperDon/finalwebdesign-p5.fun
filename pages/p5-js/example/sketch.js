883d1ae1540d778e093edfe6e090eeabfb5efee2
TheKooperDon <valodinejr@gmail.com>
2023-04-30 21:32:55 -0500

    added 2 examples for placeholders

---
+++ pages/p5-js/example/sketch.js
@@ -0,0 +1,41 @@
+
+
+function setup() {
+  createCanvas(500, 500);
+
+}
+function draw() {
+
+  ellipse(mouseX, mouseY, 50, 50);
+  fill(random(255), random(255), random(255))
+}
+
+  /*
+var x = 0;
+  while (x <= width) {
+    xc
+    x = x + 50;
+
+  }
+
+  ellipse(0, 200, 25, 25);
+  x = x + 50;
+
+  ellipse(50, 200, 25, 25);
+  x = x + 50;
+
+  ellipse(100, 200, 25, 25);
+  x = x + 50;
+
+  ellipse(150, 200, 25, 25);
+  x = x + 50;
+
+  ellipse(200, 200, 25, 25);
+  x = x + 50;
+
+  ellipse(250, 200, 25, 25);
+  x = x + 50;
+
+  //ellipse(300, 200, 25, 25);
+
+*/
