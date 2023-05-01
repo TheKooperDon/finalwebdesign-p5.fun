ab57038a7ed825990d1a418a2259de47c1bb5b76
TheKooperDon <valodinejr@gmail.com>
2023-04-30 21:50:14 -0500

    examples-hero done.

--- pages/p5-js/examples-hero/sketch.js
+++ pages/p5-js/examples-hero/sketch.js
@@ -1,33 +1,17 @@
-let sun;
-let buildings = [];
+let angle = 0;
 
 function setup() {
-  createCanvas(800, 600);
-  sun = new Sun();
-  for (let i = 0; i < 10; i++) {
-    buildings.push(new Building(random(0, width), random(200, height), random(50, 200)));
-  }
+  createCanvas(800, 400);
 }
 
 function draw() {
-  let skyColor = color(250, 110, 240);
-  let sunColor = color(255, 230, 120);
-  let buildingColor = color(50, 50, 50);
-  
-  // Draw the sky
-  background(skyColor);
-  
-  // Draw the sun
-  sun.display(sunColor);
-  
-  // Draw the buildings
-  for (let i = 0; i < buildings.length; i++) {
-    buildings[i].display(buildingColor);
+  background(0);
+  angle += 0.05;
+  for (let y = 0; y < height; y++) {
+    let wave = sin(angle + y * 0.02);
+    let xoffset = map(wave, -1, 1, -50, 50);
+    let bright = map(wave, -1, 1, 0, 255);
+    stroke(bright, bright, bright);
+    line(0 + xoffset, y, width + xoffset, y);
   }
-  
-  // Change the color of the sky, sun, and buildings over time
-  let cycleTime = map(sin(frameCount / 100), -1, 1, 0, 1);
-  skyColor = lerpColor(color(250, 110, 240), color(135, 206, 235), cycleTime);
-  sunColor = lerpColor(color(255, 230, 120), color(255, 180, 0), cycleTime);
-  buildingColor = lerpColor(color(50, 50, 50), color(200, 200, 200), cycleTime);
 }
