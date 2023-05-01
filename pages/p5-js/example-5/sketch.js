ecae84c77f599b3a4243b8fa75ae6902b7cdb9bd
TheKooperDon <valodinejr@gmail.com>
2023-05-01 14:57:12 -0500

    not sure whats up but please work

--- pages/p5-js/example-5/sketch.js
+++ pages/p5-js/example-5/sketch.js
@@ -1,5 +1,6 @@
 let particles = [];
 let attractor;
+let time = 0;
 
 function setup() {
   createCanvas(300, 400);
@@ -10,7 +11,7 @@ function setup() {
 
 function draw() {
   background(200, 50, 50);
-  
+
   // Attract and repel particles from the attractor
   for (let i = 0; i < particles.length; i++) {
     let force = attractor.attract(particles[i]);
@@ -21,15 +22,18 @@ function draw() {
       particles.splice(i, 1);
     }
   }
-  
+
   // Update and show the attractor
   attractor.update();
   attractor.show();
-  
+
   // Add new particles on mouse press
   if (mouseIsPressed) {
     particles.push(new Particle(mouseX, mouseY));
   }
+
+  // Update time for color change
+  time += 0.01;
 }
 
 class Particle {
@@ -38,7 +42,8 @@ class Particle {
     this.vel = createVector(random(-1, 1), random(-5, -1));
     this.acc = createVector(0, 0);
     this.life = 255;
-    this.hue = random(180, 250);
+    this.startHue = random(0, 360);
+    this.endHue = (this.startHue + 180) % 360;
     this.size = random(5, 20);
   }
 
@@ -50,7 +55,8 @@ class Particle {
   }
 
   show() {
-    fill(this.hue, 100, 100, this.life);
+    let hue = lerpColor(color(this.startHue, 100, 100), color(this.endHue, 100, 100), this.life/255.0);
+    fill(hue);
     ellipse(this.pos.x, this.pos.y, this.size, this.size);
   }
 
@@ -62,24 +68,23 @@ class Particle {
     this.acc.add(force);
   }
 }
-
 class Attractor {
   constructor(x, y) {
     this.pos = createVector(x, y);
     this.mass = 50;
     this.size = 20;
   }
-  
+
   update() {
     this.pos.x = mouseX;
     this.pos.y = mouseY;
   }
-  
+
   show() {
     fill(0, 100, 100);
     ellipse(this.pos.x, this.pos.y, this.size, this.size);
   }
-  
+
   attract(particle) {
     let force = p5.Vector.sub(this.pos, particle.pos);
     let distance = force.mag();
@@ -88,7 +93,7 @@ class Attractor {
     force.setMag(strength);
     return force;
   }
-  
+
   repel(particle) {
     let force = p5.Vector.sub(particle.pos, this.pos);
     let distance = force.mag();
@@ -97,4 +102,4 @@ class Attractor {
     force.setMag(strength);
     return force;
   }
-}
+} 
