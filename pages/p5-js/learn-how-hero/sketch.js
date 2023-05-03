6fe0b76564af0b065b6843eb3435d74722fc887f
TheKooperDon <valodinejr@gmail.com>
2023-05-03 10:03:15 -0500

    last hero

--- pages/p5-js/learn-how-hero/sketch.js
+++ pages/p5-js/learn-how-hero/sketch.js
@@ -1,41 +1,88 @@
-let colors = [];
-let numCircles = 20;
-let circleSize = 100;
-let circleSpacing = 50;
+// Define the variables for the ball and the paddles
+let ballX = 400;
+let ballY = 300;
+let ballSpeedX = 5;
+let ballSpeedY = 5;
+let ballSize = 20;
+let paddleWidth = 20;
+let paddleHeight = 100;
+let leftPaddleY = 250;
+let rightPaddleY = 250;
+let paddleSpeed = 10;
 
 function setup() {
+  // Create a canvas of size 800 by 600
   createCanvas(800, 600);
-  noStroke();
-  
-  // create an array of random colors for the circles
-  for (let i = 0; i < numCircles; i++) {
-    colors[i] = color(random(200, 255), random(200, 255), random(200, 255));
-  }
 }
 
 function draw() {
-  // draw background gradient
-  background(200, 100, 100);
-  for (let i = 0; i < height; i++) {
-    let from = color(255, 200, 0);
-    let to = color(0, 200, 255);
-    let inter = lerpColor(from, to, i / height);
-    stroke(inter);
-    line(0, i, width, i);
+  // Fill the background with black
+  background(0);
+  
+  // Draw the ball with white color
+  fill(255);
+  ellipse(ballX, ballY, ballSize, ballSize);
+  
+  // Draw the left paddle with white color
+  rect(0, leftPaddleY, paddleWidth, paddleHeight);
+  
+  // Draw the right paddle with white color
+  rect(width - paddleWidth, rightPaddleY, paddleWidth, paddleHeight);
+  
+  // Move the ball by its speed
+  ballX += ballSpeedX;
+  ballY += ballSpeedY;
+  
+  // Check if the ball hits the top or bottom edge and reverse its vertical speed
+  if (ballY < ballSize / 2 || ballY > height - ballSize / 2) {
+    ballSpeedY = -ballSpeedY;
   }
   
-  // update circle colors
-  for (let i = 0; i < numCircles; i++) {
-    colors[i] = lerpColor(colors[i], color(random(200, 255), random(200, 255), random(200, 255)), 0.01);
+  // Check if the ball hits the left or right edge and reset its position and speed
+  if (ballX < ballSize / 2 || ballX > width - ballSize / 2) {
+    ballX = width / 2;
+    ballY = height / 2;
+    ballSpeedX = random(-5, 5);
+    ballSpeedY = random(-5, 5);
   }
   
-  // draw circles
-  let xOffset = (width - (numCircles * (circleSize + circleSpacing))) / 2;
-  let yOffset = height / 2;
-  for (let i = 0; i < numCircles; i++) {
-    let x = xOffset + (i * (circleSize + circleSpacing));
-    let y = yOffset + sin(frameCount / 50 + i) * 50;
-    fill(colors[i]);
-    ellipse(x, y, circleSize, circleSize);
+  // Check if the ball hits the left paddle and reverse its horizontal speed
+  if (ballX < paddleWidth + ballSize / 2 && 
+      ballY > leftPaddleY && 
+      ballY < leftPaddleY + paddleHeight) {
+    ballSpeedX = -ballSpeedX;
+    // Add some randomness to the vertical speed
+    ballSpeedY += random(-1, 1);
   }
+  
+   // Check if the ball hits the right paddle and reverse its horizontal speed
+   if (ballX > width - paddleWidth - ballSize / 2 && 
+      ballY > rightPaddleY && 
+      ballY < rightPaddleY + paddleHeight) {
+    ballSpeedX = -ballSpeedX;
+    // Add some randomness to the vertical speed
+    ballSpeedY += random(-1, 1);
+   }
+   
+   // Move the left paddle up or down based on the key pressed
+   if (keyIsDown(UP_ARROW)) {
+     leftPaddleY -= paddleSpeed;
+   }
+   if (keyIsDown(DOWN_ARROW)) {
+     leftPaddleY += paddleSpeed;
+   }
+   
+   // Constrain the left paddle within the canvas
+   leftPaddleY = constrain(leftPaddleY, 0, height - paddleHeight);
+   
+   // Move the right paddle up or down based on the position of the ball
+   if (ballY < rightPaddleY + paddleHeight / 2) {
+     rightPaddleY -= paddleSpeed;
+   }
+   if (ballY > rightPaddleY + paddleHeight / 2) {
+     rightPaddleY += paddleSpeed;
+   }
+   
+   // Constrain the right paddle within the canvas
+   rightPaddleY = constrain(rightPaddleY, 0, height - paddleHeight);
 }
